e0472233b02e2789f12f8c07686d16cb537e9939
o-reo <eruaud@student.le-101.fr>
2019-10-21 16:18:13 +0200

    Seperating files catching errors

---
+++ tests/test_solving.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import sys
+import unittest
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from npuzzle import Parser, Solver
+
+class TestSolving(unittest.TestCase):
+
+    def test_solvable(self):
+        for i in range(1, 6):
+            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'puzzles/s_{}.puz'.format(i))) as input_file:
+                parse = Parser()
+                while parse.push(input_file.readline()):
+                    pass
+                solver = Solver(None)
+                is_solved = solver.solve(parse)
+                self.assertTrue(is_solved and solver.n_moves < 155)
+
+    def test_unsolvable(self):
+        for i in range(1, 6):
+            with open(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'puzzles/u_{}.puz'.format(i))) as input_file:
+                parse = Parser()
+                while parse.push(input_file.readline()):
+                    pass
+                solver = Solver(None)
+                is_solved = solver.solve(parse)
+                self.assertFalse(is_solved)
