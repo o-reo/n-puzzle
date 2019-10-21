ea099ea400e3a2f67af38042084f86fff1c66b73
Arthur Matthys <amatthys@z1r6p6.le-101.fr>
2019-10-21 14:59:55 +0200

    after merge parsing

---
+++ solver/old_parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import sys
+import os
+
+PY_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+sys.path.append(PY_DIR)
+
+class Parser():
+    def __init__(self):
+        self.status = False
+        pass
+
+    def push(self, input_line):
+        pass
--- solver/parser.py
+++ solver/parser.py
@@ -1,15 +1,81 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import sys
-import os
 
-PY_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-sys.path.append(PY_DIR)
+def is_int(line):
+    for char in line:
+        if char not in "0123456789":
+            return 0
+    return 1
 
-class Parser():
+class SizeNotFound(Exception):
+    pass
+
+class SizeTooSmall(Exception):
+    pass
+
+class BadInput(Exception):
+    pass
+
+class NotIntegerFound(Exception):
+    pass
+
+class WrongNumberOfInteger(Exception):
+    pass
+
+class Parser:
+
     def __init__(self):
-        self.status = False
-        pass
+        self.size = -1
+        self.nbr_line = 0
+        self.array = []
 
-    def push(self, input_line):
-        pass
+    def get_size(self, line):
+        if (len(line) and is_int(line)):
+            self.size = int(line)
+
+    def get_puzzle(self, line):
+        nbr_integer = 0
+        integers = line.split()
+        if len(integers) != self.size:
+            raise BadInput 
+        for integer in integers:
+            if not is_int(integer):
+                raise NotIntegerFound
+            else:
+                self.array.append(integer)
+                nbr_integer += 1
+        if (nbr_integer != self.size):
+            raise WrongNumberOfInteger
+        self.nbr_line += 1
+
+    def push(self, line, end = False):
+        line = line.split('#')[0]
+        if end:
+            if self.size == -1:
+                raise SizeNotFound
+            if self.size < 3:
+                raise SizeTooSmall
+            if self.size != self.nbr_line:
+                raise BadInput
+        if self.size == -1:
+            self.get_size(line)
+        else:
+            self.get_puzzle(line)
+
+if __name__ == "__main__":
+    parser = Parser()
+    end = False
+    while (True):
+        try:
+            line = input()
+        except EOFError:
+            end = True 
+        parser.push(line, end)
+        if parser.size == parser.nbr_line:
+            try:
+                line = input()
+            except EOFError:
+                break
+            raise BadInput
+    print("puzzle : ", parser.array)
+    print("size : ", parser.size)
