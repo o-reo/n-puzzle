e7112aa0c56dd1f723530a6e49484339f91b3719
o-reo <eruaud@student.le-101.fr>
2019-10-23 18:41:51 +0200

    Profiling option

--- __main__.py
+++ __main__.py
@@ -5,11 +5,13 @@ from npuzzle import *
 import argparse
 import time
 import cProfile
+import sys
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--heuristic", "-H", type=str, help="heuristic to use", default="manhattan")
     parser.add_argument("--search", "-s", type=str, help="heuristic to use", default="greedy")
+    parser.add_argument("--profiling", "-p", help="profiling functions", action="store_true")
 
     return parser.parse_args()
 
@@ -26,5 +28,18 @@ if __name__ == "__main__":
             break
     parser.build()
     solver = Solver(parser.numpize(), args)
-    # cProfile.run("solver.solve()")
-    print(solver.solve())
+    if args.profiling:
+    # PROFILING
+        def solving():
+            print(solver.solve())
+        cProfile.run('solving()', sort='tottime')
+        sys.exit()
+    a = solver.solve()
+    b2 = time.time()
+    print(b2-b1)
+    print("coups a faire : {}".format(a[0][1]))
+    print("nombre coups a faire : {}".format(len(a[0][1])))
+    print("nombre etat ouvert en meme temps max : {}".format(a[1]))
+    print("nombre etat ouvert : {}".format(a[2]))
+
+    
